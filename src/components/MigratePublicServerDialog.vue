164f7e3f2798197b0abfc52c765ae19e681d45d5
petervhood <peter@vpnhood.com>
2024-03-13 00:16:31 +0330

    Add MigratePublicServerDialog.vue for VpnHood (Complete)

--- src/components/MigratePublicServerDialog.vue
+++ src/components/MigratePublicServerDialog.vue
@@ -7,71 +7,62 @@
       :scrollable="true"
       :persistent="true"
   >
-    <v-card  class="px-5 pt-5">
-      <v-btn
-          rounded="pill"
-          variant="text"
-          color="primary"
-          :text="$t('CLOSE')"
-          @click="$emit('update:modelValue',false)"
-      />
-      <v-card-title class="text-center pt-0 pb-2">Migration of the free public server</v-card-title>
+    <v-card>
+
+      <v-card-title class="text-center pt-0 pb-2 text-primary" style="background-color: #f2f2f2;">
+        <v-btn icon="mdi-window-close" variant="tonal"  size="small" class="d-block mx-auto my-4" @click="$emit('update:modelValue',false)"/>
+       <span>Migration of the VpnHood public server</span>
+      </v-card-title>
       <v-divider/>
 
-      <v-card-text class="text-body-2 px-2">
-        <p>
-          For informing users about the migration of the free public server from VpnHood to VpnHoodConnect and setting expectations regarding ads, it's crucial to communicate clearly, concisely, and with a focus on user benefit. Here's a suggested prompt that balances these aspects:
-        </p>
-        <v-alert class="mb-3" type="warning" text="Important Update: Your Free VPN Service is Moving!"></v-alert>
-        <p>
-          Dear VpnHood User,
-
+      <v-card-text class="text-body-2 px-5">
+        <p><strong>Dear VpnHood User,</strong></p>
+        <p class="mb-3">
           We're excited to announce that our free public servers are getting an upgrade! To provide you with a better, more robust VPN experience, we're transitioning our free services from VpnHood to a new, dedicated app: "VpnHood! Connect".
         </p>
-        <h5>What's New?</h5>
-        <ul>
-          <li><strong>Enhanced Performance:</strong><br/> Enjoy faster, more reliable connections.</li>
-          <li><strong>Streamlined Access:</strong><br/> Easier navigation and user interface.</li>
-          <li><strong>Ad-Supported Service:</strong><br/> To keep your VPN service free, VpnHood Connect will feature occasional ads, ensuring we can continue offering top-notch security without a price tag.</li>
+        <h2>What's New?</h2>
+        <ul style="list-style: disc;" class="ps-5 mb-3">
+          <li><strong>Enhanced Performance:</strong> Enjoy faster, more reliable connections.</li>
+          <li><strong>Streamlined Access:</strong> Easier navigation and user interface.</li>
+          <li><strong>Ad-Supported Service:</strong> To keep your VPN service free, VpnHood Connect will feature occasional ads, ensuring we can continue offering top-notch security without a price tag.</li>
         </ul>
 
-        <h5>What Do I Need to Do?</h5>
-        <ol>
-          <li>Download *VpnHood Connect* from Google Play.</li>
-          <li>Log in with your existing VpnHood credentials.</li>
-          <li>Connect and enjoy! Your favorite servers, now with improvements.</li>
-        </ol>
-        <p>
+        <h2>What Do I Need to Do?</h2>
+        <p>Download <strong>VpnHood! Connect</strong> from Google Play.</p>
+        <p class="my-3">
           We understand changes can be inconvenient, but we're here to make this transition as smooth as possible. Our support team is standing by for any questions or assistance you may need.
-          <br/>
+          </p>
+        <p class="mb-3">
           Thank you for your trust in VpnHood. We look forward to continuing to protect your online privacy and security with VpnHood Connect.
-          <br/>
-          Warm regards,
+        </p>
+        <p class="mb-3">
+          Warm regards,<br/>
           The VpnHood Team
         </p>
-        <p class="text-disabled">
+
+        <p class="text-gray-lighten-2 text-caption font-italic">
           This message aims to inform users about the migration, introduce the concept of ads as a necessity for a free service, and guide them through the transition process, all while maintaining a positive tone about the changes.
         </p>
       </v-card-text>
 
-      <v-card-actions>
+      <v-card-actions class="bg-background">
+        <v-spacer/>
         <!-- Accept button -->
         <v-btn
-            color="tertiary"
             variant="flat"
             size="small"
             text="Download VpnHood! Connect"
             href="https://play.google.com/store/apps/details?id=com.vpnhood.connect.android"
-            class="text-primary-darken-2 text-capitalize"
+            target="_blank"
+            class="text-capitalize"
+            color="secondary"
         />
-        <v-spacer/>
         <v-btn
-            color="tertiary"
             variant="tonal"
             size="small"
             text="Continue"
             @click="continueConnect"
-            class="text-primary-darken-2 text-capitalize"
+            class="text-capitalize"
         />
       </v-card-actions>
     </v-card>
