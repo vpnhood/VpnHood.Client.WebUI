8746adbc20f3a9d9bdc635b710835970755d17c8
Trudy <trudy@vpnhood.com>
2025-10-20 03:56:54 -0700

    fix(Proxies): update click handler for proxies settings item to use named route

--- src/pages/settings/index.vue
+++ src/pages/settings/index.vue
@@ -115,7 +115,7 @@ function isShowConnectivitySectionTitle(): boolean {
       :isPremium="false"
       :is-show="true"
       :selectedItem="proxyModeLabel"
-      :click="{path: '/proxies'}"
+      :click="{name:'PROXIES'}"
     />
 
 
