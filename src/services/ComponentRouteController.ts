26f99a38bb54bef5d3a497ee5925e7db4bc758fc
Trudy <trudy@vpnhood.com>
2025-11-29 22:20:29 -0800

    fix(ComponentRouteController): improve delay handling and component navigation logic

--- src/services/ComponentRouteController.ts
+++ src/services/ComponentRouteController.ts
@@ -25,15 +25,15 @@ export class ComponentRouteController {
     }
 
     private static delay(ms: number) {
-      return new Promise(resolve => setTimeout(resolve, ms));
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
     public static async showComponent(componentName: string, value: boolean = true): Promise<void> {
         const showLock: AsyncLock = new AsyncLock();
         await showLock.acquire("showLock", async () => {
-          await this.showComponentInternal(componentName, value);
-
-          //show component by delay to make sure previous back is finished
-          await this.delay(value ? 200 : 100);
+            // wait to make sure we are not in the middle of navigation
+            // v-model may call showComponentin middle of navigation
+            await this.delay(100); 
+            await this.showComponentInternal(componentName, value);
         });
     }
     private static async showComponentInternal(componentName: string, value: boolean): Promise<void> {
@@ -45,13 +45,16 @@ export class ComponentRouteController {
             await router.push({ path: route.path, query: { ...route.query, [componentName]: "true" } });
             window.document.title = componentName;
         } else {
-          // make sure remove the route (because back may not work)
-          const query = { ...route.query };
-          delete query[componentName];
-          await router.replace({ path: route.path, query: query });
+            // make sure remove the component query param before call router.back()
+            // because router.back() is not async, and this method will return without removing the query param
+            // so loop may occur
+            const query = { ...route.query };
+            delete query[componentName];
+             await router.replace({ path: route.path, query: query });
 
-          // remove the route from history
-          router.back();
+            // remove the route from browser history
+            /// router.replace can not remove the history entry
+            router.back();
         }
     }
 }
