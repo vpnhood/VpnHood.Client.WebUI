63f57f941235b23fe09a84d7e477f273a8fd4f61
Peter <peter@vpnhood.com>
2025-12-12 05:06:18 +0330

    Enhance proxy management: - Add delete proxy functionality with confirmation dialog. - Refactor proxy add/edit forms for modularity and improved field validation. - Update localization keys (`REMOVE_PROXY`, `REMOVE_PROXY_MSG`).

--- src/components/Proxies/AddOrEditProxy.vue
+++ src/components/Proxies/AddOrEditProxy.vue
@@ -1,10 +1,11 @@
 ﻿<script setup lang="ts">
-import { ProxyEndPoint, ProxyProtocol } from '@/services/VpnHood.Client.Api';
+import { AppProxyEndPointInfo, ProxyEndPoint } from '@/services/VpnHood.Client.Api';
 import { VpnHoodApp } from '@/services/VpnHoodApp';
 import i18n from '@/locales/i18n';
 import { ProxySheetType } from '@/components/Proxies/ProxyUtils';
-import { ref, toRef, watch } from 'vue';
+import { computed, ref, toRef, watch } from 'vue';
 import { Validators } from '@/helpers/Validators';
+import ProxyFields from '@/components/Proxies/ProxyFields.vue';
 
 const vhApp = VpnHoodApp.instance;
 const locale = i18n.global.t;
@@ -20,41 +21,79 @@ const emit = defineEmits<{
   (e: 'loadProxies'): void;
 }>();
 
-
-const actionButtonText = ref<string>(props.proxyType === ProxySheetType.edit ? 'OK' : 'ADD');
 const proxy = toRef(props, 'selectedProxyEndPoint');
-
-const protocolItems = [
-  { value: ProxyProtocol.Http, title: 'http' },
-  { value: ProxyProtocol.Https, title: 'https' },
-  { value: ProxyProtocol.Socks4, title: 'socks4' },
-  { value: ProxyProtocol.Socks5, title: 'socks5' }
-];
-
-const isShowAuthenticationFields = ref<boolean>(
-  !Validators.isEmptyString(proxy.value.username) || !Validators.isEmptyString(proxy.value.password)
-);
-
-// Reset username and password field when the isShowAuthenticationFields switch state changed
-watch(isShowAuthenticationFields, async (newVal, oldVal) => {
-  if (oldVal === true && newVal === false) {
-    proxy.value.username = '';
-    proxy.value.password = ''
-  }
+const isFieldDirty = ref(false);
+const isProcessing = ref(false);
+const oldProxyId = ref('');
+const proxyList = ref('');
+const isAddButtonAvailable = computed((): boolean => {
+  if (isProcessing.value) return false;
+  if (props.proxyType === ProxySheetType.addList) return !Validators.isEmptyString(proxyList.value);
+  return fieldRules.host() === true && fieldRules.port() === true;
 });
+
+const fieldRules = {
+  host: () => !Validators.isEmptyString(proxy.value.host) || locale('PROXY_REQUIRED_HOST'),
+  port: () => proxy.value.port > 0 && proxy.value.port <= 65535 || locale('PROXY_INVALID_PORT')
+};
+
+watch(() => props.modelValue, (isOpen) => {
+  if (isOpen) {
+    // Reset fields when opening
+    oldProxyId.value = props.selectedProxyEndPoint.id;
+  }
+})
+
+async function processHostField(): Promise<void> {
+  isFieldDirty.value = true;
+  if (Validators.isEmptyString(proxy.value.host) || fieldRules.port() !== true || isProcessing.value)
+    return;
+
+  try {
+    isProcessing.value = true;
+    const result: AppProxyEndPointInfo = await vhApp.proxyEndPointClient.parse(proxy.value.host.trim(), proxy.value);
+    Object.assign(proxy.value, result.endPoint);
+    isFieldDirty.value = false;
+  } finally {
+    isProcessing.value = false;
+  }
+}
+
 async function addOrUpdateProxy(){
-  console.log(typeof proxy.value);
-  console.log(typeof proxy.value.port);
-  console.log(proxy.value.port);
-  return;
-  if (props.proxyType === ProxySheetType.add) {
-    await vhApp.proxyEndPointClient.add(proxy.value);
+  isProcessing.value = true;
+  switch (props.proxyType) {
+    case ProxySheetType.add:
+      await vhApp.proxyEndPointClient.add(proxy.value);
+      break;
+    case ProxySheetType.addList:
+      await vhApp.proxyEndPointClient.import(proxyList.value);
+      break;
+    default:
+      await vhApp.proxyEndPointClient.update(oldProxyId.value, proxy.value);
+      break;
   }
-  else {
-    await vhApp.proxyEndPointClient.update(proxy.value.id, proxy.value);
+  closeSheet();
+}
+
+async function deleteProxy(): Promise<void> {
+  if (!await vhApp.showConfirmDialog(locale('REMOVE_PROXY'), locale('REMOVE_PROXY_MSG')))
+    return;
+
+  try {
+    isProcessing.value = true;
+    await vhApp.proxyEndPointClient.delete(proxy.value.id);
+    closeSheet();
   }
-  emit('loadProxies');
+  finally {
+    isProcessing.value = false;
+  }
+}
+
+
+function closeSheet(){
   emit('update:modelValue',false);
+  emit('loadProxies');
+  isProcessing.value = false;
 }
 
 </script>
@@ -67,86 +106,63 @@ async function addOrUpdateProxy(){
     max-width="100%"
   >
 
-    <config-card color="background" class="rounded-b-0" >
+    <config-card color="background" class="rounded-b-0" :disabled="isProcessing" :loading="isProcessing">
 
       <v-card-item>
-        <h4 class="border-b pb-3 mb-2">{{ locale(props.proxyType) }}</h4>
+        <div class="d-flex align-center justify-space-between border-b pb-3 mb-2">
+          <!-- Page title -->
+          <h4>{{ locale(props.proxyType) }}</h4>
 
-        <div class="d-flex align-center justify-space-between">
-          <span>{{ locale('PROXY_ENABLED') }}</span>
-          <v-switch v-model="proxy.isEnabled" density="compact" hide-details />
+          <!-- Remove button on the edit mode -->
+          <v-btn
+            v-if="props.proxyType === ProxySheetType.edit"
+            :text="locale('REMOVE')"
+            variant="text"
+            density="comfortable"
+            color="error"
+            @click="deleteProxy()"
+          />
         </div>
 
-        <v-text-field
-          v-model="proxy.host"
-          :label="locale('PROXY_HOST')"
+        <!-- Add proxy list -->
+        <p class="text-caption text-disabled mb-3">{{locale('PROXY_IMPORT_DESC')}}</p>
+        <v-textarea
+          v-if="props.proxyType === ProxySheetType.addList"
+          v-model="proxyList"
+          :label="locale('PROXY_IMPORT_LABEL')"
+          variant="outlined"
           hide-details="auto"
-          variant="outlined"
-          density="comfortable"
-          rounded="lg"
-          color="highlight"
-          class="mt-4"
+          auto-grow
+          rows="6"
         />
 
-        <v-text-field
-          v-model="proxy.port"
-          :label="locale('PROXY_PORT')"
-          type="number"
-          variant="outlined"
-          density="comfortable"
-          rounded="lg"
-          color="highlight"
-          class="mt-4"
+        <!-- Add or update proxy fields -->
+        <proxy-fields
+          v-else
+          :field-rules="fieldRules"
+          :proxy-end-point="proxy"
+          @process-host-field="processHostField"
         />
 
-        <v-select
-          v-model="proxy.protocol"
-          :items="protocolItems"
-          item-title="title"
-          item-value="value"
-          variant="outlined"
-          density="comfortable"
-          rounded="lg"
-          color="highlight"
-          :label="locale('PROXY_PROTOCOL')"
-        />
-
-        <div class="d-flex align-center justify-space-between">
-          <span>{{ locale('AUTHENTICATION') }}</span>
-          <v-switch v-model="isShowAuthenticationFields" density="compact" hide-details/>
-        </div>
-
-        <template v-if="isShowAuthenticationFields">
-          <v-text-field
-            v-model="proxy.username"
-            :label="locale('PROXY_USERNAME')"
-            hide-details="auto"
-            variant="outlined"
-            density="comfortable"
-            rounded="lg"
-            color="highlight"
-            class="mt-4"
-          />
-
-          <v-text-field
-            v-model="proxy.password"
-            :label="locale('PROXY_PASSWORD')"
-            hide-details="auto"
-            variant="outlined"
-            density="comfortable"
-            rounded="lg"
-            color="highlight"
-            class="mt-4"
-            type="password"
-          />
-        </template>
       </v-card-item>
 
       <v-card-actions>
-        <v-btn :text="locale('CANCEL')" variant="plain" @click="emit('update:modelValue',false)" />
-        <v-btn :text="locale(actionButtonText)" @click="addOrUpdateProxy()" />
-      </v-card-actions>
 
+        <!-- Cancel button -->
+        <v-btn
+          :text="locale('CANCEL')"
+          variant="plain"
+          @click="emit('update:modelValue',false)"
+        />
+
+        <!-- Add or update button -->
+        <v-btn
+          :text="locale(props.proxyType === ProxySheetType.edit ? 'OK' : 'ADD')"
+          :disabled="!isAddButtonAvailable"
+          @click="addOrUpdateProxy()"
+        />
+
+      </v-card-actions>
     </config-card>
   </v-bottom-sheet>
 </template>
--- src/components/Proxies/ProxyFields.vue
+++ src/components/Proxies/ProxyFields.vue
@@ -1,19 +1,18 @@
 ﻿<script setup lang="ts">
-import { computed, ref, watch } from 'vue';
+import { ref, toRef, watch } from 'vue';
 import i18n from '@/locales/i18n';
 import { ProxyEndPoint, ProxyProtocol } from '@/services/VpnHood.Client.Api';
 import { Validators } from '@/helpers/Validators';
-import { ProxyUtil } from '@/components/Proxies/ProxyUtils';
 
 const locale = i18n.global.t;
 
-const proxy = computed({
-  get: () => ProxyUtil.proxyEndPoint,
-  set: (value: ProxyEndPoint) => {
-    console.log(value);
-    ProxyUtil.proxyEndPoint = value
-  }
-})
+const props = defineProps<{
+  proxyEndPoint: ProxyEndPoint,
+  fieldRules: { host: () => true|string; port: () => true|string }
+}>()
+const emit = defineEmits<{
+  (e: 'process-host-field'): void
+}>()
 
 const protocolItems = [
   { value: ProxyProtocol.Http, title: 'http' },
@@ -22,30 +21,73 @@ const protocolItems = [
   { value: ProxyProtocol.Socks5, title: 'socks5' }
 ];
 
-
-const isShowAuthenticationFields = ref<boolean>(
-  !Validators.isEmptyString(proxy.value.username) || !Validators.isEmptyString(proxy.value.password)
-);
+const proxy = toRef(props, 'proxyEndPoint');
+const isShowAuthenticationFields = ref(!Validators.isEmptyString(proxy.value.username) ||
+  !Validators.isEmptyString(proxy.value.password));
 
 // Reset username and password field when the isShowAuthenticationFields switch state changed
 watch(isShowAuthenticationFields, async (newVal, oldVal) => {
-  if (oldVal === true && newVal === false) {
+  if (oldVal && !newVal) {
     proxy.value.username = '';
     proxy.value.password = ''
   }
-})
-
+});
 </script>
 
 <template>
-    <div class="d-flex align-center justify-space-between">
-      <span>{{ locale('PROXY_ENABLED') }}</span>
-      <v-switch v-model="proxy.isEnabled" density="compact" hide-details />
-    </div>
 
+  <div class="d-flex align-center justify-space-between">
+    <span>{{ locale('PROXY_ENABLED') }}</span>
+    <v-switch v-model="proxy.isEnabled" density="compact" hide-details />
+  </div>
+
+  <v-text-field
+    v-model="proxy.host"
+    :label="locale('PROXY_HOST')"
+    @blur="emit('process-host-field')"
+    :rules="[props.fieldRules.host]"
+    hide-details="auto"
+    variant="outlined"
+    density="comfortable"
+    rounded="lg"
+    color="highlight"
+    class="mt-4"
+  />
+
+  <v-text-field
+    v-model.number="proxy.port"
+    :label="locale('PROXY_PORT')"
+    :rules="[props.fieldRules.port]"
+    hide-details="auto"
+    type="number"
+    variant="outlined"
+    density="comfortable"
+    rounded="lg"
+    color="highlight"
+    class="my-4"
+  />
+
+  <v-select
+    v-model="proxy.protocol"
+    :items="protocolItems"
+    item-title="title"
+    item-value="value"
+    variant="outlined"
+    density="comfortable"
+    rounded="lg"
+    color="highlight"
+    :label="locale('PROXY_PROTOCOL')"
+  />
+
+  <div class="d-flex align-center justify-space-between">
+    <span>{{ locale('AUTHENTICATION') }}</span>
+    <v-switch v-model="isShowAuthenticationFields" density="compact" hide-details/>
+  </div>
+
+  <template v-if="isShowAuthenticationFields">
     <v-text-field
-      v-model="proxy.host"
-      :label="locale('PROXY_HOST')"
+      v-model="proxy.username"
+      :label="locale('PROXY_USERNAME')"
       hide-details="auto"
       variant="outlined"
       density="comfortable"
@@ -55,55 +97,14 @@ watch(isShowAuthenticationFields, async (newVal, oldVal) => {
     />
 
     <v-text-field
-      v-model="proxy.port"
-      :label="locale('PROXY_PORT')"
-      type="number"
+      v-model="proxy.password"
+      :label="locale('PROXY_PASSWORD')"
+      hide-details="auto"
       variant="outlined"
       density="comfortable"
       rounded="lg"
       color="highlight"
       class="mt-4"
     />
-
-    <v-select
-      v-model="proxy.protocol"
-      :items="protocolItems"
-      item-title="title"
-      item-value="value"
-      variant="outlined"
-      density="comfortable"
-      rounded="lg"
-      color="highlight"
-      :label="locale('PROXY_PROTOCOL')"
-    />
-
-    <div class="d-flex align-center justify-space-between">
-      <span>{{ locale('AUTHENTICATION') }}</span>
-      <v-switch v-model="isShowAuthenticationFields" density="compact" hide-details/>
-    </div>
-
-    <template v-if="isShowAuthenticationFields">
-      <v-text-field
-        v-model="proxy.username"
-        :label="locale('PROXY_USERNAME')"
-        hide-details="auto"
-        variant="outlined"
-        density="comfortable"
-        rounded="lg"
-        color="highlight"
-        class="mt-4"
-      />
-
-      <v-text-field
-        v-model="proxy.password"
-        :label="locale('PROXY_PASSWORD')"
-        hide-details="auto"
-        variant="outlined"
-        density="comfortable"
-        rounded="lg"
-        color="highlight"
-        class="mt-4"
-        type="password"
-      />
-    </template>
+  </template>
 </template>
