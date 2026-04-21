8f78e08c0cd5b627ef11f06656202b51f507fd8b
Trudy <trudy@vpnhood.com>
2026-04-21 13:56:17 -0700

    refactor: remove unused FeaturePageLayout component and simplify template structure

--- src/pages/split-tunneling/index.vue
+++ src/pages/split-tunneling/index.vue
@@ -5,7 +5,6 @@ import { VpnHoodApp } from '@/services/VpnHoodApp';
 import i18n from '@/locales/i18n';
 import { AppFeature } from '@/services/VpnHood.Client.Api';
 import SmallFeatureImageAndDescription from '@/components/Settings/SmallFeatureImageAndDescription.vue';
-import FeaturePageLayout from '@/components/Settings/FeaturePageLayout.vue';
 import SettingsItem from '@/components/Settings/SettingsItem.vue';
 import SettingsSectionTitle from '@/components/Settings/SettingsSectionTitle.vue';
 
@@ -15,16 +14,7 @@ const locale = i18n.global.t;
 
 <template>
 
-  <feature-page-layout
-    v-if="!vhApp.data.isPremiumFeatureAllowed(AppFeature.SplitByIpViaDevice)"
-    title="SPLIT_IP_ADDRESSES_COLORED"
-    description="SPLIT_IP_ADDRESSES_DESC"
-    image="split-ip.webp"
-    :is-premium="vhApp.data.isPremiumFeature(AppFeature.SplitByIpViaDevice)"
-    :is-action-button-available="false"
-  />
-
-  <v-sheet v-else>
+  <v-sheet>
     <app-bar/>
 
     <small-feature-image-and-description image="split-ip.webp" :description="locale('SPLIT_TUNNELING_DESC')" />
